0e1843e1d139d0b9e9311bd76c898939d0ca8df7
hrz394943230 <394943230@qq.com>
2022-11-04 18:13:51 +0800

    修改index模板
    强调了callback里面的模板id 和userid

--- config/index.cjs
+++ config/index.cjs
@@ -72,13 +72,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '0002',
+  CALLBACK_TEMPLATE_ID: '',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'PDU17561TWdzPR0aP6HEGtwcj2tyhQbGs83p2Zl0q',
+      id: '',
     }
   ],
 
